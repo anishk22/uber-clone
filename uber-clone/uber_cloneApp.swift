c51feb8b1553b4d9b92aaac3fc916e8362a43f3b
Anish Kothapalli <anishvk@umich.edu>
2023-05-14 15:24:03 -0400

    Initial Commit

---
+++ uber-clone/uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_cloneApp.swift
+//  uber-clone
+//
+//  Created by Anish Kothapalli on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
