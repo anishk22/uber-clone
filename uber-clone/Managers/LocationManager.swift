1575f285948a57260ae8fe477316b03830ff7b35
Anish Kothapalli <anishvk@umich.edu>
2023-05-14 16:34:55 -0400

    current user location now shows up

---
+++ uber-clone/Managers/LocationManager.swift
@@ -0,0 +1,27 @@
+//
+//  LocationManager.swift
+//  uber-clone
+//
+//  Created by Anish Kothapalli on 5/14/23.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
