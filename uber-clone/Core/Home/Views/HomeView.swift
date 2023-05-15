f3c2ae440e43e1470714230960ccdc779aa6b822
Anish Kothapalli <anishvk@umich.edu>
2023-05-15 13:30:48 -0400

    added where to UI

---
+++ uber-clone/Core/Home/Views/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  uber-clone
+//
+//  Created by Anish Kothapalli on 5/14/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            HStack {
+                Spacer()
+                
+                MapViewActionButton()
+                
+                Spacer()
+                
+                LocationSearchActivationView()
+                
+                Spacer()
+            }
+            .padding(.top, 36)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
