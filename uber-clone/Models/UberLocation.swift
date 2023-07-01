686dce89535c9ca2b0668dfc24cbc5fb01515df1
Anish Kothapalli <anishvk@umich.edu>
2023-07-01 16:18:13 -0500

    added pickup + dropoff info

---
+++ uber-clone/Models/UberLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UberLocation.swift
+//  uber-clone
+//
+//  Created by Anish Kothapalli on 7/1/23.
+//
+
+import Foundation
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
